c71dd5c3609394ade002cc4dcd42f493b403fdad
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-05 21:49:08 -0800

    product detail page now displaying details through redux

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -2,7 +2,11 @@ import axios from 'axios'
 import { 
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
-    PRODUCT_LIST_FAIL
+    PRODUCT_LIST_FAIL,
+
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL
  } from '../constants/productConstants'
 
  export const listProducts = () => async (dispatch) => {
@@ -24,3 +28,24 @@ import {
         })
     }
  }
+
+ export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
