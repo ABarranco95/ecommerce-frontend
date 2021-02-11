34cee773fc11b3d62e15e55b860399e2082e3e6f
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-11 13:46:40 -0800

    added progress bar for checkout process

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
@@ -25,6 +26,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2/>
       <Form onSubmit={submitHandler}>
         <h1>Shipping</h1>
         <Form.Group controlId="address">
