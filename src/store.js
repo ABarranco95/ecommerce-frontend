23257147fd2bf8b86d9e04b0e33d6fe32b432f21
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-12 14:42:05 -0800

    Create order functionality and cart clear after purchase

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
-
+import { orderCreateReducers } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -14,6 +14,7 @@ const reducer = combineReducers({
     userRegister : userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducers,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
