46a421aa00cccfa3588f6e67c4565e82b2cc5f25
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-15 13:14:38 -0800

    Delete product from admin screen functionality added

--- src/screens/ProductListScreen.js
+++ src/screens/ProductListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listProducts } from "../actions/productActions";
+import { listProducts, deleteProduct } from "../actions/productActions";
 
 
 const ProductListScreen = ({ history, match }) => {
@@ -12,6 +12,9 @@ const ProductListScreen = ({ history, match }) => {
   
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+  
+  const productDelete = useSelector((state) => state.productDelete);
+  const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -23,11 +26,11 @@ const ProductListScreen = ({ history, match }) => {
       } else{
           history.push('/login')
       }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
       if(window.confirm('Are you sure you want to delete this product?')){
-        //   delete
+        dispatch(deleteProduct(id));
       }
   }
 
@@ -48,6 +51,9 @@ const ProductListScreen = ({ history, match }) => {
             </Button>
           </Col>
       </Row>
+
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
 
       {loading 
         ? (<Loader />) 
