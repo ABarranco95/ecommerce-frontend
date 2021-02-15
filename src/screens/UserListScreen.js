d8d8ffc3db82deb55983d54e53d4f55697fcc6c2
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-14 16:40:41 -0800

    Admin can delete users

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 
 
 const UserListScreen = ({ history }) => {
@@ -16,16 +16,21 @@ const UserListScreen = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
       if(userInfo && userInfo.isAdmin){
           dispatch(listUsers());
       } else{
           history.push('/login')
       }
-  }, [dispatch, history]);
+  }, [dispatch, history, successDelete]);
 
   const deleteHandler = (id) => {
-      console.log('delete:', id);
+      if(window.confirm('Are you sure you want to delete this user?')){
+          dispatch(deleteUser(id))
+      }
   }
 
   return (
