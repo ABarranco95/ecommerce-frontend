31ecbdcff73adffa491e2f8220e2e79337137fbd
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-16 17:40:56 -0800

    Add admin order view screen

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import PaymentScreen from "./screens/PaymentScreen"
 import PlaceOrderScreen from "./screens/PlaceOrderScreen"
 import OrderScreen from "./screens/OrderScreen"
 import UserListScreen from "./screens/UserListScreen"
+import OrderListScreen from "./screens/OrderListScreen"
 import UserEditScreen from "./screens/UserEditScreen"
 import ProductListScreen from "./screens/ProductListScreen"
 import ProductEditScreen from "./screens/ProductEditScreen"
@@ -36,6 +37,7 @@ function App() {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
