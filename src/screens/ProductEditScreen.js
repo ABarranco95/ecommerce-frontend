5f95f0ca4f08a2270f5b0e7d29bfa87049cc00a1
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2021-02-16 17:14:56 -0800

    add image upload functionality

--- src/screens/ProductEditScreen.js
+++ src/screens/ProductEditScreen.js
@@ -1,54 +1,62 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import FormContainer from "../components/FormContainer";
-import { listProductDetails, updateProduct } from "../actions/productActions";
-import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+import { Link } from "react-router-dom"
+import { Form, Button } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
+import Loader from "../components/Loader"
+import Message from "../components/Message"
+import FormContainer from "../components/FormContainer"
+import { listProductDetails, updateProduct } from '../actions/productActions'
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants"
 
 const ProductEditScreen = ({ match, history }) => {
-  const productId = match.params.id;
 
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState(0);
-  const [image, setImage] = useState("");
-  const [brand, setBrand] = useState("");
-  const [category, setCategory] = useState("");
-  const [countInStock, setCountInStock] = useState(0);
-  const [description, setDescription] = useState("");
+  const productId = match.params.id
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState(0)
+  const [image, setImage] = useState('')
+  const [brand, setBrand] = useState('')
+  const [category, setCategory] = useState('')
+  const [countInStock, setCountInStock] = useState(0)
+  const [description, setDescription] = useState('');
+  const [uploading, setUploading] = useState(false)
 
   const dispatch = useDispatch();
 
   const productDetails = useSelector((state) => state.productDetails);
   const { error, loading, product } = productDetails;
 
-  const productUpdate = useSelector((state) => state.productUpdate);
+  const productUpdate = useSelector((state) => state.productUpdate)
   const {
     error: errorUpdate,
     loading: loadingUpdate,
     success: successUpdate,
-  } = productUpdate;
+  } = productUpdate
 
-  useEffect(() => {
-    if (successUpdate) {
-      dispatch({ type: PRODUCT_UPDATE_RESET });
-      history.push("/admin/productlist");
-    } else {
-      if (!product.name || product._id !== Number(productId)) {
-        dispatch(listProductDetails(productId));
-      } else {
-        setName(product.name);
-        setPrice(product.price);
-        setImage(product.image);
-        setBrand(product.brand);
-        setCategory(product.category);
-        setCountInStock(product.countInStock);
-        setDescription(product.description);
-      }
-    }
-  }, [dispatch, product, productId, history, successUpdate]);
+    useEffect(() => {
+
+        if (successUpdate) {
+            dispatch({ type: PRODUCT_UPDATE_RESET })
+            history.push('/admin/productlist')
+        } else {
+            if (!product.name || product._id !== Number(productId)) {
+                dispatch(listProductDetails(productId))
+            } else {
+                setName(product.name)
+                setPrice(product.price)
+                setImage(product.image)
+                setBrand(product.brand)
+                setCategory(product.category)
+                setCountInStock(product.countInStock)
+                setDescription(product.description)
+
+            }
+        }
+
+
+
+    }, [dispatch, product, productId, history, successUpdate])
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -63,6 +71,33 @@ const ProductEditScreen = ({ match, history }) => {
         description,
     }))
   };
+
+  const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+
+        formData.append('image', file)
+        formData.append('product_id', productId)
+
+        setUploading(true)
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+
+            const { data } = await axios.post('/api/products/upload/', formData, config)
+
+
+            setImage(data)
+            setUploading(false)
+
+        } catch (error) {
+            setUploading(false)
+        }
+    }
 
   return (
     <div>
@@ -106,7 +141,19 @@ const ProductEditScreen = ({ match, history }) => {
                 placeholder="Enter image"
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
-              ></Form.Control>
+              >  
+              </Form.Control>
+
+              <Form.File 
+                id='image-file' 
+                label='Choose File'
+                custom
+                onChange={uploadFileHandler}
+              >
+
+              </Form.File>
+              
+
             </Form.Group>
 
             <Form.Group controlId="brand">
@@ -132,7 +179,7 @@ const ProductEditScreen = ({ match, history }) => {
             <Form.Group controlId="category">
               <Form.Label>Category</Form.Label>
               <Form.Control
-                type="ntext"
+                type="text"
                 placeholder="Enter category"
                 value={category}
                 onChange={(e) => setCategory(e.target.value)}
